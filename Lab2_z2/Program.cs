6f21c3a313c790ae835fc347b773cceefaa08be1
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2023-07-03 04:32:19 +0200

    Task solved

--- Lab2_z2/Program.cs
+++ Lab2_z2/Program.cs
@@ -1,3 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace Lab2_z2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite cijeli broj n:");
+            string unos = Console.ReadLine();
+            int n = Int32.Parse(unos);
+
+            int x = n - 1, y = n - 1;
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < 2*n - 1; j++)
+                {
+                    if(j >= x && j <= y)
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+
+                }
+                Console.WriteLine("");
+                x--;
+                y++;
+            }
+        }
+    }
+}
